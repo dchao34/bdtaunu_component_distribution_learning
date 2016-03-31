248e1eb876549b981b33ccacc24f2aecf5e04697
dchao <dchao@caltech.edu>
2016-03-30 22:49:16 -0700

    Added mle fit that can use different sensitivity cache file.

---
+++ component_learning/sensitivity_selection/alpha_search_cvx.py
@@ -0,0 +1,64 @@
+#! /local/dchao/software/anaconda2/bin/python
+
+import time
+import numpy as np
+from iopro import genfromtxt
+from mle_cvxnl import perform_mle
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cache_suffix', type=str, default='.csv',
+                        help='suffix of the cache files. ')
+    parser.add_argument('--obj_scale', type=float, default=1e-8,
+                        help='scale factor to apply to objective function.')
+    parser.add_argument('--bootstrap', action='store_true',
+                        help='whether to bootstrap the data. ')
+    parser.add_argument('cache_dir', type=str, 
+                        help='directory holding the cache files. ')
+    parser.add_argument('n_components', type=int, 
+                        help='number of components. ')
+    parser.add_argument('alphas', type=str, nargs='*',
+                        help='sensitiviy parameters.')
+    args = parser.parse_args()
+
+    # assemble cached data based on sensitivity list
+    print '+ assembling cached records. '
+    start = time.time()
+
+    cache_fname = args.cache_dir + '/' + args.alphas[0] + args.cache_suffix
+    arr = genfromtxt(cache_fname)
+
+    if len(args.alphas) == 1:
+        pass
+    else:
+        if len(args.alphas) != args.n_components:
+            raise RuntimeError('alphas must have either 1 or the same number of '
+                               'arguments as what is specified in n_components. ')
+
+        for i in range(1, args.n_components):
+            curr_cache_fname = args.cache_dir + '/' + args.alphas[i] + args.cache_suffix
+            curr_arr = genfromtxt(curr_cache_fname)
+            arr[:,i] = curr_arr[:,i]
+
+    N, D = arr.shape
+    if args.bootstrap:
+        arr = arr[np.random.choice(N, N)]
+
+    end = time.time()
+    print '  running time: {0} s. \n'.format(end - start)
+
+    # solve maximum likelihood
+    print '+ solving maximum likelihood. \n'
+    start = time.time()
+    sol = perform_mle(arr, args.obj_scale)
+    end = time.time()
+
+    # Report results
+    print
+    print '  Solver status: {0}'.format(sol['status'])
+    print '  Minimum: {0}'.format(sol['primal objective'])
+    print '  Minimizers:', np.array(sol['x']).reshape(-1)
+    print
+    print '  running time: {0} s. \n'.format(end - start)
--- component_learning/sensitivity_selection/mle_cvxnl.py
+++ component_learning/sensitivity_selection/mle_cvxnl.py
@@ -1,4 +1,5 @@
 #! /local/dchao/software/anaconda2/bin/python
+
 import sys
 import math
 import numpy as np
@@ -11,6 +12,65 @@ from iopro import genfromtxt
 # 0.41817119150694954
 # 0.07374540140202024
 # 0.49265798277959727
+def perform_mle(p, obj_scale=1e-8):
+
+    # get cache dimensions
+    N, D = p.shape
+
+    # Specify the objective function and its derivatives
+    def F(x=None, z=None):
+
+        # Optionally scale the objective to avoid numerical difficulties
+        s = obj_scale
+
+        # Handle call signature F()
+        if x is None:
+            return 0, cvx.matrix(1.0/D, (D, 1))
+
+        # Handle call signature F(x)
+        x_arr = np.array(x.trans()).reshape(-1)
+        arg = np.dot(p, x_arr)
+
+        if np.min(arg) <= 0.0:
+            return None
+
+        f = -np.sum(np.log(arg))
+        f = cvx.matrix(f) * s
+
+        arg_m1 = np.power(arg, -1.0)
+        Df = [ -np.dot(arg_m1, p[:,c]) for c in range(D) ]
+        Df = cvx.matrix(Df, (1, D)) * s
+
+        if z is None:
+            return f, Df
+
+        # Handle call signature F(x, z)
+        arg_m2 = -2 * np.log(arg)
+        H = np.zeros((D, D))
+        for b in range(D):
+            for c in range(D):
+                H[b, c] = np.sum(np.exp(arg_m2 + np.log(p[:,b]) + np.log(p[:,c]))) * z[0]
+        H = cvx.matrix(H) * s
+
+        return f, Df, H
+
+    # Specify the inequality constraints
+    G = cvx.matrix(np.diag(-np.ones(D)))
+    h = cvx.matrix(np.zeros(D))
+
+    # Specify the equality constraints
+    A = cvx.matrix(np.ones(D), (1, D))
+    b = cvx.matrix(1.0)
+
+    # Specify required metadata
+    dims = { 'l': D, 'q': [], 's': [] }
+
+    # Solve
+    sol = cvx.solvers.cp(F, G, h, dims, A, b)
+
+    return sol
+
+
 if __name__ == '__main__':
 
     import argparse
@@ -40,61 +100,13 @@ if __name__ == '__main__':
         # Create a bagged sample.
         p = p_raw[np.random.choice(N, N)]
 
-        # Specify the objective function and its derivatives
-        def F(x=None, z=None):
-
-            # Optionally scale the objective to avoid numerical difficulties
-            s = args.obj_scale
-
-            # Handle call signature F()
-            if x is None:
-                return 0, cvx.matrix(0.2, (D, 1))
-
-            # Handle call signature F(x)
-            x_arr = np.array(x.trans()).reshape(-1)
-            arg = np.dot(p, x_arr)
-
-            if np.min(arg) <= 0.0:
-                return None
-
-            f = -np.sum(np.log(arg))
-            f = cvx.matrix(f) * s
-
-            arg_m1 = np.power(arg, -1.0)
-            Df = [ -np.dot(arg_m1, p[:,c]) for c in range(D) ]
-            Df = cvx.matrix(Df, (1, D)) * s
-
-            if z is None:
-                return f, Df
-
-            # Handle call signature F(x, z)
-            arg_m2 = -2 * np.log(arg)
-            H = np.zeros((D, D))
-            for b in range(D):
-                for c in range(D):
-                    H[b, c] = np.sum(np.exp(arg_m2 + np.log(p[:,b]) + np.log(p[:,c]))) * z[0]
-            H = cvx.matrix(H) * s
-
-            return f, Df, H
-
-        # Specify the inequality constraints
-        G = cvx.matrix(np.diag(-np.ones(D)))
-        h = cvx.matrix(np.zeros(D))
-
-        # Specify the equality constraints
-        A = cvx.matrix(np.ones(D), (1, D))
-        b = cvx.matrix(1.0)
-
-        # Specify required metadata
-        dims = { 'l': D, 'q': [], 's': [] }
-
         # Solve
-        sol = cvx.solvers.cp(F, G, h, dims, A, b)
+        sol = perform_mle(p, args.obj_scale)
 
         # Report results
         print
         print 'Solver status: {0}'.format(sol['status'])
-        print 'Minimum: {0}'.format(F(sol['x'])[0][0])
+        print 'Minimum: {0}'.format(sol['primal objective'])
         print 'Minimizers:', np.array(sol['x']).reshape(-1)
         print
         print
@@ -105,9 +117,3 @@ if __name__ == '__main__':
 
     # Cleanup
     fout.close()
-
-    print [ 0.004597168626601867, 
-            0.009011011183443553,
-            0.41817119150694954,
-            0.07374540140202024,
-            0.49265798277959727 ]
