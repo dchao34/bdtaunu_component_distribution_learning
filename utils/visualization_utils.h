1154e743d4ad45a9f7486062a4feeace2ee26624
dchao <dchao@caltech.edu>
2016-04-07 16:02:40 -0700

    Factored out general utilities.

--- utils/visualization_utils.h
+++ utils/visualization_utils.h
@@ -6,17 +6,6 @@
 #include <string>
 #include <utility>
 
-// read weighted 2d points from file `fname`. each row is corresponds to 
-// a point. the last column is the weight; otherwise, the value in column `i`
-// is the value of coodinate `i`. 
-template<typename PointT>
-std::vector<PointT> read_2dpoints(const std::string &fname);
-
-// returns true if lhs < rhs reverse lexicographically
-template<typename PointT>
-bool ReverseExactLexicoLess(const PointT &lhs, const PointT &rhs);
-
-
 // generate a 1-dimensional grid of points. 
 template <typename PointT>
 void generate_1dgrid(std::vector<PointT> &grid, 
